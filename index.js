4a4751b5abc4f2f89ce7e4bef7c563c072b55b56
valentinagonzalezF <v.gonzalez.2@ug.uchile.cl>
2021-03-23 12:32:21 -0300

    Add logger

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-require("dotenv").config();
+const morgan = require('morgan');
+const logger = require('./logger')
 
+// dotenv
+require("dotenv").config();
 const port = process.env.PORT;
 const host = process.env.HOST;
-// dotenv
 
 const app = express();
 
@@ -13,10 +15,17 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+app.use(morgan('[:date[iso]] :method :url :status :res[content-length] - :response-time ms'));
+
+// Routes
 const routes = require("./src/routes/routes");
 
 app.use("/api", routes);
 
-app.listen(port, () =>
-  console.log(`Respira Confianza api listening at ${host}:${port}`)
+app.listen(port, () => {
+  logger.info(`Server running at ${host}:${port}`);
+  //logger.warn('And fits on your back?');
+  //logger.error('Its log, log, log');
+}
 );
---
+++ logger.js
@@ -0,0 +1,19 @@
+const { createLogger, format, transports } = require('winston');
+require("dotenv").config();
+const NODE_ENV = process.env.NODE_ENV;
+
+// Logger
+module.exports = createLogger({
+    level: NODE_ENV === 'development' ? 'debug' : 'info',
+    format: format.combine(
+        NODE_ENV === 'development' ? format.colorize() : format.uncolorize(),
+        format.simple(),
+        format.timestamp(),
+        format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`),
+    ),
+    transports: [
+        new transports.Console({
+            level: 'debug'
+        })
+    ]
+})
