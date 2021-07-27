af920338693e737ca986602a7af1d03d37f847dc
valentinagonzalezF <v.gonzalez.2@ug.uchile.cl>
2021-07-26 23:22:37 -0400

    Readings: add pagination and filters

--- src/controllers/station_readings.js
+++ src/controllers/station_readings.js
@@ -1,14 +1,25 @@
 const models = require('../models');
 
 const getStationReadings = async (req, res) => {
-  //TODO: PAGINATION
-  const readings = await models.Station_Readings.findAll({
+  const limit = req.query.limit || 10;
+  const page = req.query.page || 0;
+  const offset = page * limit;
+  const station = req.query.station || -1;
+
+  let options = {
+    limit,
+    offset,
     include: {
       model: models.Station
     },
     order: [['recorded_at', 'DESC']]
-  });
-  return res.status(200).json({ readings })
+  };
+  if (station != -1) {
+    options.where = { station_id: station }
+  }
+
+  const readings = await models.Station_Readings.findAndCountAll(options);
+  return res.status(200).json({ readings: readings.rows, countAllReadings: readings.count })
 };
 
 module.exports = {
