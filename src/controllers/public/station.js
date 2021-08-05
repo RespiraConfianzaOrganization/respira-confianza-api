168286ee8488e518e3ba08dfa8c852775b4b45ac
valentinagonzalezF <v.gonzalez.2@ug.uchile.cl>
2021-08-05 18:20:37 -0400

    :hammer:

--- src/controllers/public/station.js
+++ src/controllers/public/station.js
@@ -292,7 +292,6 @@ const recommendedStations = async (req, res) => {
   let stations = await models.Station.findAll({
     attributes: ['id', [sequelize.literal("6371 * acos(cos(radians(" + latitude + ")) * cos(radians(latitude)) * cos(radians(" + longitude + ") - radians(longitude)) + sin(radians(" + latitude + ")) * sin(radians(latitude)))"), 'distance']],
     order: sequelize.col('distance'),
-    limit: 3,
   });
 
   stations = stations.map(station => station.id)
