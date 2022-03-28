70df84d90e4a60d132e79ca6ac77ed69ef6b3b80
David Escobar <david.escobar@unholster.com>
2022-03-28 15:41:24 -0300

    Fix typos

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     dialect: 'postgres',
     logging: false
   },
