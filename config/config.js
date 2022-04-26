52139fd68f8bf78d78a9b27496b899393e527313
David Escobar <david.escobar@unholster.com>
2022-04-26 17:48:00 -0400

    avg hourly

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: "127.0.0.1",
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: false
   },
   test: {
--- src/controllers/public/pollutant_station_readings.js
+++ src/controllers/public/pollutant_station_readings.js
@@ -1,32 +1,61 @@
 const models = require("../../models");
 const { Op } = require('sequelize');
-const moment = require("moment");
+const sequelize = require("sequelize");
+
+const makeQuery = (startDate, endDate, stations, fields) => {
+    const stringFields = fields.map(f => `AVG("${f}") AS ${f}`)
+    const joinedFields = stringFields.join(',')
+    const tupleStations = `(${stations.map(s => `'${s}'`).toString()})`
+    const query = `
+    SELECT station_id,
+           make_timestamp(CAST(DATE_PART('year', recorded_at) as integer),
+                          CAST(DATE_PART('month', recorded_at) as integer),
+                          CAST(DATE_PART('day', recorded_at) as integer),
+                          CAST(DATE_PART('hour', recorded_at) as integer),
+                          0,
+                          0
+           ) as TIMESTAMP,
+           ${joinedFields}
+    FROM "Station_Readings"
+    WHERE recorded_at BETWEEN '${startDate}' AND '${endDate}'
+          AND station_id IN ${tupleStations}
+    GROUP BY station_id,
+             DATE_PART('day', recorded_at),
+             DATE_PART('hour', recorded_at),
+             DATE_PART('year', recorded_at),
+             DATE_PART('month', recorded_at)
+    `
+
+    return query
+}
 
 const pollutantsReadingsByStations = async (req, res) => {
 
-    const { pollutants, stations, days } = req.body;
+    const { pollutants, stations, startDate, endDate } = req.body;
     const pollutantsAttributes = pollutants || []
     const attributes = ["id", "station_id", "recorded_at", ...pollutantsAttributes]
 
-    const endDate = moment()
-    const startDate = moment().subtract(days, 'days')
+    const query = makeQuery(startDate, endDate, stations, pollutants)
+    const queryToExecute = query
+    // const stationsObjects = await models.Station_Readings.findAll({
+    //     where: {
+    //         station_id: stations,
+    //         recorded_at: {
+    //             [Op.between]: [startDate, endDate],
+    //         }
+    //     },
+    //     attributes: attributes
+    // });
 
-    const endDateISO = endDate.toISOString()
-    const startDateISO = startDate.toISOString()
-
-    const stationsObjects = await models.Station_Readings.findAll({
-        where: {
-            station_id: stations,
-            recorded_at: {
-                [Op.between]: [startDateISO, endDateISO],
-            }
-        },
-        attributes: attributes
+    const stationsObjects = await models.sequelize.query(queryToExecute, {
+        raw: true,
+        type: sequelize.QueryTypes.SELECT,
     });
+
 
     const groupedByStation = {}
     stations.forEach(station => {
-        groupedByStation[station] = stationsObjects.filter(({station_id}) => station_id === station)
+        groupedByStation[station] = stationsObjects.filter(({station_id}) => station_id.toLowerCase() === station.toLowerCase())
     })
 
     return res.status(200).json({ readings: groupedByStation});
