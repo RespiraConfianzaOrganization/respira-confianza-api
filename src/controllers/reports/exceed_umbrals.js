7a6785ba336c46397cfc16dcec4b7e130dd27a2f
David Escobar <david.escobar@unholster.com>
2022-05-18 18:50:08 -0400

    Small refactor

--- src/controllers/reports/exceed_umbrals.js
+++ src/controllers/reports/exceed_umbrals.js
@@ -1,7 +1,5 @@
 const validators = require("./validators");
-const {getThresholdByPollutant, getPollutant, getStation} = require("./queries")
-const models = require("../../models");
-const {Op} = require("sequelize");
+const {getThresholdByPollutant, getPollutant, getStation, getTimesExceedThreshold} = require("./queries")
 
 const fs = require("fs");
 const path = require("path");
@@ -34,29 +32,10 @@ const getErrors = ({startDate, endDate, pollutant, station}) => {
     return errors
 }
 
-const getTimesExceedThreshold = async ({stationId, minThreshold, maxThreshold, pollutant, startDate, endDate}) => {
-    const query = {
-        station_id: stationId,
-        recorded_at: {
-            [Op.between]: [startDate, endDate],
-        },
-    }
-    query[pollutant] = {[Op.between]: [minThreshold, maxThreshold]}
-
-    const attributes = ['station_id', 'recorded_at', 'updated_at', [pollutant, 'value']]
-
-    return await models.Station_Readings.findAll({
-        where: query,
-        attributes: attributes,
-        raw: true,
-        nest: true
-    })
-}
-
 const getConsecutivePairs = array => {
     const output = []
-    for(let i = 0; i < array.length - 1; i++) {
-        const currentPair = [array[i], array[i+1]]
+    for (let i = 0; i < array.length - 1; i++) {
+        const currentPair = [array[i], array[i + 1]]
         output.push(currentPair)
     }
     return output
@@ -66,6 +45,16 @@ const makeThresholdPairsByPollutant = async (thresholds) => {
     const {good, moderate, unhealthy, very_unhealthy} = thresholds
     const thresholdsValues = [0, good, moderate, unhealthy, very_unhealthy, Infinity]
     return getConsecutivePairs(thresholdsValues)
+}
+
+const getLabel = ({isLastValue, isFirstValue, minValue, maxValue}) => {
+    if (isFirstValue) {
+        return `Menor a ${maxValue}`
+    } else if (isLastValue) {
+        return `Mayor a ${minValue}`
+    } else {
+        return `${minValue} - ${maxValue}`
+    }
 }
 
 const getReportDataPerPollutantAndStation = async ({pollutant, station, startDate, endDate}) => {
@@ -78,46 +67,31 @@ const getReportDataPerPollutantAndStation = async ({pollutant, station, startDat
         endDate: endDate,
         requestDate: startDate
     }
-
     const thresholdsPairs = await makeThresholdPairsByPollutant(thresholdData)
-
     let ranges = []
     let globalResults = []
-
-    const queryParams = {
-        stationId: station,
-        pollutant: pollutant,
-        startDate: startDate,
-        endDate: endDate,
-    }
-
     for (let i = 0; i < thresholdsPairs.length; i++) {
         const [minValue, maxValue] = thresholdsPairs[i]
         const results = await getTimesExceedThreshold({
-            ...queryParams,
+            stationId: station,
+            pollutant: pollutant,
+            startDate: startDate,
+            endDate: endDate,
             minThreshold: minValue,
             maxThreshold: maxValue
         })
-
-        globalResults = globalResults.concat(results)
-
-        let rangeKey = ''
-
-        if (i === 0) {
-            rangeKey = `Menor a ${maxValue}`
-        } else if (i === thresholdsPairs.length - 1) {
-            rangeKey = `Mayor a ${minValue}`
-        } else {
-            rangeKey = `${minValue} - ${maxValue}`
-        }
-
+        const label = getLabel({
+            isLastValue: i === thresholdsPairs.length - 1,
+            isFirstValue: i === 0,
+            minValue: minValue,
+            maxValue: maxValue
+        })
         ranges.push({
-            key: rangeKey,
+            key: label,
             count: results.length
         })
-
+        globalResults = globalResults.concat(results)
     }
-
     return {
         pollutant: pollutantData,
         station: stationData,
@@ -128,12 +102,11 @@ const getReportDataPerPollutantAndStation = async ({pollutant, station, startDat
     }
 }
 
-const createPDF = async (data) => {
-    const templatePath = path.join(process.cwd(), 'src', 'static', 'exceedThresholdsTemplate.html')
+const createPDF = async (data, templatePath) => {
     const templateHtml = fs.readFileSync(templatePath, 'utf8')
     const template = handlebars.compile(templateHtml)
-    const htmlContent = { content: template(data) }
-    return await htmlToPdf.generatePdf(htmlContent, { format: 'A4' })
+    const htmlContent = {content: template(data)}
+    return await htmlToPdf.generatePdf(htmlContent, {format: 'A4'})
 }
 
 const exceedThresholdController = async (req, res) => {
@@ -141,7 +114,8 @@ const exceedThresholdController = async (req, res) => {
     const hasErrors = Object.keys(errors).length > 0
     if (hasErrors) return res.status(400).json({message: errors})
     const reportData = await getReportDataPerPollutantAndStation({...req.body})
-    const reportPDF = await createPDF(reportData)
+    const templatePath = path.join(process.cwd(), 'src', 'static', 'exceedThresholdsTemplate.html')
+    const reportPDF = await createPDF(reportData, templatePath)
     res.contentType('application/pdf')
     return res.send(reportPDF)
 }
--- src/controllers/reports/queries.js
+++ src/controllers/reports/queries.js
@@ -1,4 +1,5 @@
 const models = require("../../models");
+const {Op} = require("sequelize");
 
 const getThresholdByPollutant = async (pollutant) => {
     return await models.Umbrals.findOne({
@@ -28,6 +29,25 @@ const getStation = async (id) => {
     })
 }
 
+const getTimesExceedThreshold = async ({stationId, minThreshold, maxThreshold, pollutant, startDate, endDate}) => {
+    const query = {
+        station_id: stationId,
+        recorded_at: {
+            [Op.between]: [startDate, endDate],
+        },
+    }
+    query[pollutant] = {[Op.between]: [minThreshold, maxThreshold]}
+
+    const attributes = ['station_id', 'recorded_at', 'updated_at', [pollutant, 'value']]
+
+    return await models.Station_Readings.findAll({
+        where: query,
+        attributes: attributes,
+        raw: true,
+        nest: true
+    })
+}
+
 module.exports = {
-    getThresholdByPollutant, getPollutant, getStation
+    getThresholdByPollutant, getPollutant, getStation, getTimesExceedThreshold
 }
