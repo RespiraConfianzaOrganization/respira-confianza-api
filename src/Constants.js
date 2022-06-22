a6e5fe2412f4e93a072eb80918fb68e772f46221
David Escobar <david.escobar@unholster.com>
2022-06-22 17:07:17 -0400

    Use module.exports instead of export

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,12 @@
-export const colors = {
+const colors = {
     LessThanGood: '#2cba00',
     BetweenGoodAndModerate: '#a3ff00',
     BetweenModerateAndUnhealthy: '#fff400',
     BetweenUnhealthyAndVeryUnhealthy: '#ffa700',
     BetweenVeryUnhealthyAndDangerous: '#CD2323FF',
     MoreThanDangerous: '#ff0000'
+}
+
+module.exports = {
+    colors
 }
